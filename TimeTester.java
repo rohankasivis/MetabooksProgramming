21f2d746dda8522a85ce5162d06ce1d5d61982c1
rohankasivis <rohan.kasivis@gmail.com>
2015-06-24 22:13:36 -0700

    These two programs run a clock that will print out the time based on a given number of seconds concurrently.

---
+++ TimeTester.java
@@ -0,0 +1,9 @@
+public class TimeTester
+{
+    public static void main(String [] args) throws InterruptedException
+    {
+        (new Thread(new TimePrinter(3))).start();
+        (new Thread(new TimePrinter(5))).start();
+        (new Thread(new TimePrinter(7))).start();
+    }
+}

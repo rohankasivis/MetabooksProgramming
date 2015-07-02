b695871f757d11d471e9fbad62e59095b0a778be
rohankasivis <rohan.kasivis@gmail.com>
2015-07-02 10:18:17 -0700

    changes made

--- DocumentStream.java
+++ DocumentStream.java
@@ -1,22 +1,29 @@
 import org.joda.time.*;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class DocumentStream implements Runnable
 {
     private DateTime time;
     private File currFile;
+    private boolean [] hasRead;
+    private File [] listOfFiles;
 
     public DocumentStream(DateTime time)
     {
         this.time = time;
     }
 
-    private File [] readFiles()
+    public File [] readFiles()
     {
         File folder = new File("C:/FTPFiles");
         File[] list = folder.listFiles();
-        File[] listOfFiles = new File[list.length];
+        listOfFiles = new File[list.length];
+        hasRead = new boolean[list.length];
 
         int pos = 0;
 
@@ -32,18 +39,19 @@ public class DocumentStream implements Runnable
 
     public boolean fileExists(File file)
     {
-        String fileName = returnFileName();
+        String fileName = fileName();
 
         if(file != null)
         {
             if(file.getName().equals(fileName))
+            {
                 return true;
+            }
         }
-
         return false;
     }
 
-    public String returnFileName()
+    public String fileName()
     {
         int year = time.getYear();
         int month = time.getMonthOfYear();
@@ -57,38 +65,63 @@ public class DocumentStream implements Runnable
         fileExists(currFile);
     }
 
-    public boolean isAfterDate(File file)
+    public boolean hasReadFile(DateTime time)
     {
-        if(file != null)
+        for(int j = 0; j < listOfFiles.length; j++)
         {
-            long time = file.lastModified();
-            DateTime fileTime = new DateTime(time);
-            int fileYear = fileTime.getYear();
-            int setYear = this.time.getYear();
-            int fileMonth = fileTime.getMonthOfYear();
-            int setMonth = this.time.getMonthOfYear();
-            int fileDay = fileTime.getDayOfMonth();
-            int setDay = this.time.getDayOfMonth();
+            if(listOfFiles[j].lastModified() == time.getMillis())
+            {
+                return hasRead[j];
+            }
+        }
+        return false;
+    }
 
-            if(fileYear > setYear)
+    public boolean isAfterDate(DateTime newTime)
+    {
+        int fileYear = newTime.getYear();
+        int setYear = this.time.getYear();
+        int fileMonth = newTime.getMonthOfYear();
+        int setMonth = this.time.getMonthOfYear();
+        int fileDay = newTime.getDayOfMonth();
+        int setDay = this.time.getDayOfMonth();
+
+        if(fileYear > setYear)
+            return true;
+        else if(fileYear == setYear)
+            if(fileMonth > setMonth)
                 return true;
-            else if(fileYear == setYear)
-                if(fileMonth > setMonth)
-                    return true;
-                else if(fileMonth == setMonth)
-                    return fileDay > setDay;
-                else
-                    return false;
+            else if(fileMonth == setMonth)
+                return fileDay > setDay;
             else
                 return false;
-        }
         else
             return false;
     }
 
+    public void readFile(File file) throws IOException
+    {
+        for(int j = 0; j < listOfFiles.length; j++)
+        {
+            if(listOfFiles[j] == file)
+                hasRead[j] = true;
+        }
+        FileReader fileToRead = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileToRead);
+
+        int numLines = 0;
+        String currLine;
+        while ((currLine = reader.readLine()) != null)
+            numLines++;
+
+        reader.close();
+
+        System.out.println("Number of lines in the file: " + numLines);
+    }
+
     public void ftpFile() throws UnknownHostException
     {
-        FTPTheClient.ftpFile(returnFileName());
+        FTPTheClient.ftpFile(fileName());
     }
 
     public void logFile()
--- RunDocuments.java
+++ RunDocuments.java
@@ -1,27 +1,53 @@
 import org.joda.time.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 public class RunDocuments
 {
-    public static void main(String [] args) throws InterruptedException
+    public static void main(String [] args) throws InterruptedException, UnknownHostException, IOException
     {
         DateTime curr = DateTime.now();
         DocumentStream stream = new DocumentStream(curr);
-        if(stream.fileExists())
-            stream.ftpFile();
-        else
-        {
-            int numTimesChecked = 1;
-            while(numTimesChecked <= 6 && !stream.fileExists())
-            {
-                if(stream.fileExists())
-                    stream.ftpFile();
-                else
-                    Thread.sleep(3600000);
-            }
 
-            if(!stream.fileExists())
-                stream.logFile();
+        File [] listOfFiles = stream.readFiles();
+
+        while (curr.getHourOfDay() != 6)
+        {
+            Thread.sleep(60000);
+        }
+
+        stream.ftpFile();
+
+        while(!stream.isAfterDate(DateTime.now()))
+        {
+            Thread.sleep(36000000);
+        }
+
+        for(int j = 0; j < listOfFiles.length; j++)
+        {
+            if((listOfFiles[j].lastModified() == DateTime.now().getMillis()))
+                return;
+
+            if(listOfFiles[j] != null)
+            {
+                if (stream.fileExists(listOfFiles[j]))
+                    stream.readFile(listOfFiles[j]);
+                else
+                {
+                    int numTimesChecked = 1;
+                    while (numTimesChecked <= 6 && !stream.fileExists(listOfFiles[j]))
+                    {
+                        if (stream.fileExists(listOfFiles[j]))
+                            stream.readFile(listOfFiles[j]);
+                        else
+                            Thread.sleep(3600000);
+                    }
+
+                    if (!stream.fileExists(listOfFiles[j]))
+                        stream.logFile();
+                }
+            }
         }
     }
 }

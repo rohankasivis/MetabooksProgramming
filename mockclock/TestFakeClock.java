60e29958bbac7e9dd63e781a4413a47a32e47644
rohankasivis <rohan.kasivis@gmail.com>
2015-07-28 12:14:00 -0700

    changes made

---
+++ mockclock/TestFakeClock.java
@@ -0,0 +1,16 @@
+package mockclock;
+
+// this class is responsible for testing the fake clock, in order to make sure that
+// it implements the functionality of an actual clock using the correct time, and
+// also makes sure that the waittill function works properly
+public class TestFakeClock
+{
+    public static void main(String [] args) throws InterruptedException
+    {
+        fakeClock clock = new fakeClock(12, 41, 1);
+        Thread.sleep(10000);
+        clock.waitTill(12, 30, 2);
+        Thread.sleep(4000);
+        System.out.println(clock.getTime());
+    }
+}
--- mockclock/fakeClock.java
+++ mockclock/fakeClock.java
@@ -4,43 +4,35 @@ import org.joda.time.DateTime;
 
 public class fakeClock implements Clock
 {
-    private int seconds;
-    private int minutes;
-    private int hours;
     private int secondsDifference;
     private int minutesDifference;
     private int hoursDifference;
 
     public fakeClock(int seconds, int minutes, int hours)
     {
-        this.seconds = seconds;
-        this.minutes = minutes;
-        this.hours = hours;
+        waitTill(hours, minutes, seconds);
+    }
+
+    public void waitTill(int hours, int minutes, int seconds)
+    {
         secondsDifference = seconds - DateTime.now().getSecondOfMinute();
         minutesDifference = minutes - DateTime.now().getMinuteOfHour();
         hoursDifference = hours - DateTime.now().getHourOfDay();
     }
 
-    public void waitTill(int hours, int minutes, int seconds)
-    {
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
-    }
-
     public int getSeconds()
     {
-        return (seconds + secondsDifference) % 60;
+        return (DateTime.now().getSecondOfMinute() + secondsDifference) % 60;
     }
 
     public int getMinutes()
     {
-        return (minutes + minutesDifference) % 60;
+        return (DateTime.now().getMinuteOfHour() + minutesDifference) % 60;
     }
 
     public int getHour()
     {
-        return (hours + hoursDifference) % 24;
+        return (DateTime.now().getHourOfDay() + hoursDifference) % 24;
     }
 
     public String getTime()
